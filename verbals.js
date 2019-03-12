a37da1464d25cbb447035e5ab45ff173c0da7990
amondilado <amondilado@users.noreply.github.com>
2019-03-12 18:56:42 +0200

    sort,filter,paginate v2

---
+++ verbals.js
@@ -0,0 +1,37 @@
+var Verbals = {
+    "button": {
+        "bookNow":"book now",
+        "sendRequest":"send a request",
+        "viewDetails":"View details"
+    },
+    "button_reset":"reset",
+    "button_close":"close",
+    "vehicleItem": {
+        "group":"Group",
+        "unavailable":"FULL BOOKED FOR THIS PERIOD",
+        "suggestedPrice":"Suggested Price",
+    },
+    "sort": {
+        "priceAsc": "Price Ascending",
+        "priceDes": "Price Descending",
+        "titleAsc": "Title Ascending",
+        "titleDes": "Title Descending"
+    },
+    "pagination": {
+        prev: "Προηγούμενο",
+        next: "Επόμενο",
+        first: "Αρχή",
+        last: "Τέλος",
+        page: "Σελίδα"
+    },
+    "vehiclesAvailable":"Available cars {0} out of {1}",
+    "pricePerDay":"price/day",
+    "priceForBookingDays":"price for <strong>{0}</strong> days",
+    "priceForSuggestedDays": "price for <strong>3</strong> days",
+    "datesFromTo":"from {0} to {1}",
+    "vatIncluded": "VAT Included",
+    "results": "Results:",
+    "noFilterResults": "No results found for the given criteria. Consider selecting fewer options.",
+    "filters": "ΦΙΛΤΡΑ",
+    "today": "Today"
+};
