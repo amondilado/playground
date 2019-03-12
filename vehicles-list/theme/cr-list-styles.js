0f76739e041e2bbc16d3f00f904eec8d9383e7e5
amondilado <amondilado@users.noreply.github.com>
2019-03-13 00:18:08 +0200

    iscroll pager
    wip

--- vehicles-list/theme/cr-list-styles.js
+++ vehicles-list/theme/cr-list-styles.js
@@ -7,9 +7,9 @@ const template = html`
 <template>
     <style is="custom-style" include="shared-styles iron-flex iron-flex-alignment"></style>
     <style>
-    .vehicles-list-main{transition:opacity .2s ease-out 0s;will-change: opacity;min-height:100vh}
+    .vehicles-list-main{transition:opacity .2s ease-out 0s;will-change: opacity;height:100vh;}
+    .aside{z-index: 10}
     .fade{opacity:.5}
-    #notifications p {padding-top:20px;font-size:18px;}
 
     .display-items { width: 120px; }
     .display-item {
