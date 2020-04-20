38d9dfae98a32252a6f919f79629770845ae5b7f
amondilado <amondilado@gmail.com>
2020-04-20 23:28:23 +0300

    tagCloud demo

---
+++ js/tag-cloud/tagCloud.data.js
@@ -0,0 +1,128 @@
+const TAG_CLOUD_WEIGHTED_DATA = [{
+    "title": "lorem",
+    "priority": 0,
+    "weight": "1"
+  }, {
+    "title": "ipsum",
+    "priority": 0,
+    "weight": "2"
+  }, {
+    "title": "dolor",
+    "priority": 0,
+    "weight": "3"
+  }, {
+    "title": "sit amet",
+    "priority": 0,
+    "weight": "4"
+  }, {
+    "title": "consectetur",
+    "priority": 0,
+    "weight": "5"
+  },{
+    "title": "adipisicing",
+    "priority": 2,
+    "weight": "1"
+  }, {
+    "title": "elit",
+    "priority": 2,
+    "weight": "2"
+  }, {
+    "title": "dolores",
+    "priority": 2,
+    "weight": "3"
+  }, {
+    "title": "placeat",
+    "priority": 2,
+    "weight": "4"
+  }, {
+    "title": "accusamus",
+    "priority": 2,
+    "weight": "5"
+  },{
+    "title": "lorem",
+    "priority": 1,
+    "weight": "1"
+  }, {
+    "title": "ipsum",
+    "priority": 1,
+    "weight": "2"
+  }, {
+    "title": "dolor",
+    "priority": 1,
+    "weight": "3"
+  }, {
+    "title": "sit amet",
+    "priority": 1,
+    "weight": "4"
+  }, {
+    "title": "consectetur",
+    "priority": 1,
+    "weight": "5"
+  }
+];
+
+const TAG_CLOUD_DATA = [{
+  "title": "lorem",
+  "priority": 0
+}, {
+  "title": "lorem",
+  "priority": 0
+}, {
+  "title": "lorem",
+  "priority": 0
+}, {
+  "title": "lorem",
+  "priority": 0
+}, {
+  "title": "lorem",
+  "priority": 0
+}, {
+  "title": "sit amet",
+  "priority": 0
+}, {
+  "title": "consectetur",
+  "priority": 0
+},{
+  "title": "adipisicing",
+  "priority": 1
+}, {
+  "title": "elit",
+  "priority": 1
+}, {
+  "title": "dolores",
+  "priority": 1
+}, {
+  "title": "dolores",
+  "priority": 1
+},{
+  "title": "lorem",
+  "priority": 1
+},{
+  "title": "lorem",
+  "priority": 1
+}, {
+  "title": "accusamus",
+  "priority": 1
+},{
+  "title": "lorem",
+  "priority": 2,
+},{
+  "title": "ipsum",
+  "priority": 2,
+},{
+  "title": "ipsum",
+  "priority": 2,
+}, {
+  "title": "ipsum",
+  "priority": 2,
+}, {
+  "title": "ipsum",
+  "priority": 2,
+}, {
+  "title": "sit amet",
+  "priority": 2,
+}, {
+  "title": "consectetur",
+  "priority": 2
+}
+];
